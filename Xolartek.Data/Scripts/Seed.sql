122137f8431332ddc499bc16eb34d17ae8965fb9
Vince Gonzales <vince.gonzales@hotmail.com>
2018-05-30 08:39:10 -0700

    update database name

---
+++ Xolartek.Data/Scripts/Seed.sql
@@ -0,0 +1,72 @@
+USE [AccountDb]
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Critical Rating')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Magazine Size')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Damage')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Damage to afflicted targets')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Causes Affliction damage for 6 seconds')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Reload Speed')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Critical Damage')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Weapon Damage when aiming down sights')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Increased Building Health')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Life Leech')
+GO
+INSERT INTO [AccountDb].[dbo].[Traits] ([Description]) VALUES ('Durability')
+GO
+INSERT INTO [AccountDb].[dbo].[Rarities] ([Description]) VALUES ('Common')
+GO
+INSERT INTO [AccountDb].[dbo].[Rarities] ([Description]) VALUES ('Epic')
+GO
+INSERT INTO [AccountDb].[dbo].[Rarities] ([Description]) VALUES ('Legendary')
+GO
+INSERT INTO [AccountDb].[dbo].[Rarities] ([Description]) VALUES ('Mythic')
+GO
+INSERT INTO [AccountDb].[dbo].[Rarities] ([Description]) VALUES ('Rare')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Active Powercell')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Adhesive Resin')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Bacon')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Batteries')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Blast Powder')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Obsidian Ore')
+GO
+INSERT INTO [AccountDb].[dbo].[Materials] ([Description]) VALUES ('Sleek Mechanical Parts')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Active Powercell', '~/content/images/120px-Active_powercell_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Adhesive Resin', '~/content/images/120px-Adhesive_resin_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Bacon', '~/content/images/120px-Bacon_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Batteries', '~/content/images/120px-Batteries_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Blast Powder', '~/content/images/120px-Blast_powder_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Obsidian Ore', '~/content/images/120px-Obsidian_ore_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Sleek Mechanical Parts', '~/content/images/120px-Sleek_mechanical_parts_icon.png')
+GO
+INSERT INTO [AccountDb].[dbo].[Pictures] ([CSSClass],[Alternate],[Source]) VALUES ('img-fluid', 'Nailer', '~/content/images/T-Icon-Weapons-SK-Hydraulic-Drum-Assault-L.png')
+GO
+INSERT INTO [AccountDb].[dbo].[WeaponType] ([Description]) VALUES ('Assault Weapon')
+GO
+INSERT INTO [AccountDb].[dbo].[WeaponType] ([Description]) VALUES ('Shotgun')
+GO
+INSERT INTO [AccountDb].[dbo].[WeaponEdition] ([Description]) VALUES ('Hydraulic')
+GO
+INSERT INTO [AccountDb].[dbo].[WeaponEdition] ([Description]) VALUES ('Neon')
+GO
